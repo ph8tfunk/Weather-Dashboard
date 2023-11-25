57e832990a07281ae23da6e79d7a8a38fffbea22
Tom Paling <137108048+ph8tfunk@users.noreply.github.com>
2023-11-25 21:44:44 +0000

    added fetch function to query weathermapAPI

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,29 @@
+const api_key = "5715a2e70d43f5204093386ab1b8f071";
 
-var search = $('#search-input');
+var searchInput = $('#search-input');
 
 $('#search-button').on('click', function(event){
     event.preventDefault();
-    console.log(search.val());
+    var search = searchInput.val();
+    getGeolocation(search);
 });
+
+function getGeolocation(city){
+
+    
+    var geo_URL = "http://api.openweathermap.org/geo/1.0/direct?q="+city+"&limit=1&appid="+api_key;
+
+    fetch(geo_URL)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data){
+            console.log(data);
+        })
+
+ }
+
+
 
 
 
